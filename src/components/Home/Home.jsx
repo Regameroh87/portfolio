ef8b067db7f6c233c0240e8de24b3d1a38cee839
Rodrigo Gamero <rodrigogamero@MacBook-Air-de-Rodrigo.local>
2023-12-22 19:51:11 -0300

    cambio video por gif

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,16 @@
 import style from "../Home/Home.module.css";
 import Projects from "../Projects/Projects";
+import { useEffect } from "react";
 
 export default function Home() {
   const code = "/>";
   const keys = "<";
+
+
   return (
     <main className={style.container}>
       <section className={style.title}>
-        <video autoPlay muted preload="auto" playsInline>
-        <source src="smoke.mp4" type="video/mp4" />
-        </video>
+        <img src="smoke.gif"/>
         <div></div>
         <h1>{keys}Hello!</h1>
         <h2>I'm Rodrigo Gamero</h2>
