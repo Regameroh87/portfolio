5d0d9dfac467a87ee104eedc0e1de8f5193a3fc5
Rodrigo Gamero <rodrigogamero@MacBook-Air-de-Rodrigo.local>
2023-12-24 11:41:41 -0300

    proyecto pokemon agregado

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,32 +1,29 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLink, faVideo } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import style from "./Projects.module.css"
+import style from "./Projects.module.css";
 
-export default function Projects () {
-    return (
-        <div>
-
-      {/* //////////////////////////// MIS PROJECTOS //////////////////////////////////////////////// */}
+export default function Projects() {
+  return (
+    <div>
       <section className={style.containerProjects}>
         <div>
           <h1>SOME OF MY LATEST WORK</h1>
         </div>
 
-        <div className={style.inmuebles360}>
-
+        <article className={style.inmuebles360}>
           <div className={style.inmuebles360Description}>
             <h3>Inmuebles 360</h3>
-            <p style={{ color: "#fff" }}>
+            <p>
               The project involves the development of a web platform that
               streamlines the property rental process, enabling users to both
               rent properties and list their own for rent. The platform
               incorporates various features to enhance the user experience.
             </p>
-            <p style={{ color: "#fff" }}>
-              Some technologies: React, JavaScript, HTML, Redux, Tailwind, Bootstrap,
-              AntDesign, Node, Cloudinary, Express, Mongodb, Nodemailer,
-              Passport, Stripe, Formik...
+            <p>
+              Some technologies: React, JavaScript, HTML, Redux, Tailwind,
+              Bootstrap, AntDesign, Node, Cloudinary, Express, Mongodb,
+              Nodemailer, Passport, Stripe, Formik...
             </p>
             <div>
               <a
@@ -56,25 +53,71 @@ export default function Projects () {
           <div className={style.inmuebles360Images}>
             <div>
               <img
-                style={{ height: "50%", objectFit:"fill"  }}
+                style={{ height: "50%", objectFit: "fill" }}
                 src="./inmuebles360/home.png"
                 alt="Home"
               />
               <img
-                style={{ height: "50%", objectFit:"fill" }}
+                style={{ height: "50%", objectFit: "fill" }}
                 src="./inmuebles360/properties.png"
                 alt="Home"
               />
             </div>
             <img src="./inmuebles360/detail.png" alt="Detail" />
-            <img src="./inmuebles360/login.png" alt="Login"  />
-            <img src="./inmuebles360/admin.png" alt="Dashboard" style={{objectFit:"fill" }} />
+            <img src="./inmuebles360/login.png" alt="Login" />
+            <img
+              src="./inmuebles360/admin.png"
+              alt="Dashboard"
+              style={{ objectFit: "fill" }}
+            />
           </div>
-        </div>
 
-        {/* <div style={{ background: "lightgreen" }}>POKEMON</div>
-          <div style={{ background: "lightblue" }}>CARROUSEL IMAGENES</div> */}
+        </article>
+
+        <article className={style.pokemonPi}>
+          <div className={style.pokemonPiDescription}>
+            <h3>Project Pokemon</h3>
+            <p>
+              The project aims to deliver a complete exploration and interaction
+              experience within the Pokémon universe for users. By offering a
+              platform for users to create their personalized Pokémon, the
+              project seeks to foster creativity and individual expression.
+              Additionally, the integration and utilization of data from the
+              PokeAPI are crucial aspects of the project, ensuring the accuracy
+              and reliability of information pertaining to existing Pokémon.
+            </p>
+            <p>
+              Some technologies: React, JavaScript, HTML, CSS, Redux, Node,
+              Express, Sequelize, Postgress
+            </p>
+            <div>
+              <a
+                href="https://github.com/Regameroh87/Pokemon-PI"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faGithub} className={style.icons} />
+              </a>
+              <a
+                href="https://vimeo.com/897329226?share=copy"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faVideo} className={style.icons} />
+              </a>
+            </div>
+          </div>
+
+          <div className={style.pokemonPiImages}>
+            <img src="./pokemon-images/home.png" />
+            <img src="./pokemon-images/form.png"/>
+            <img src="./pokemon-images/detail.png" />
+            <img src="./pokemon-images/modal.png" />
+          </div>
+
+        </article>
+
       </section>
-        </div>
-    )
+    </div>
+  );
 }
